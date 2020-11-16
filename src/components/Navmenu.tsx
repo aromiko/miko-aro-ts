328d25a8bc4d2a3509f35edd738d4d4525dd7568
Miko Aro <john.aro@boltoninternational.com>
2020-11-16 22:13:41 +0800

    Update packages and imports

--- src/components/Navmenu.tsx
+++ src/components/Navmenu.tsx
@@ -1,9 +1,9 @@
-import Facebook from "../assets/images/icons/facebook.svg";
-import Instagram from "../assets/images/icons/instagram.svg";
-import Linkedin from "../assets/images/icons/linkedin.svg";
+import Facebook from "assets/images/icons/facebook.svg";
+import Instagram from "assets/images/icons/instagram.svg";
+import Linkedin from "assets/images/icons/linkedin.svg";
 import React from "react";
 import { State } from "ducks/Reducers";
-import Twitter from "../assets/images/icons/twitter.svg";
+import Twitter from "assets/images/icons/twitter.svg";
 import { useSelector } from "react-redux";
 
 const openSocialMedia = (url: string) => {
