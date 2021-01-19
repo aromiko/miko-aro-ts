938d0be55b705bde18eb3341b1daf743efd1a78c
Miko Aro <john.aro@boltoninternational.com>
2021-01-19 20:37:26 +0800

    Add tenure margin

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -21,7 +21,9 @@ const Work: React.FC<WorkPageType> = ({
         <div className="text-left text-xl lg:text-3xl my-30 lg:mb-42 lg:mt-57 lg:order-1 order-2">
           <p className="section-title mb-6">{company}</p>
           <p className="section-subtitle text-white font-light">{title}</p>
-          <h5 className="text-white text-opacity-60 font-light">{tenure}</h5>
+          <h5 className="text-white text-opacity-60 font-light mt-1">
+            {tenure}
+          </h5>
         </div>
         <div className="lg:order-2 order-1 mt-51 lg:m-0 lg:mb-42">
           <img
